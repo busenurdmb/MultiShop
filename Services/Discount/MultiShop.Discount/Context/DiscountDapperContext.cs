d098f703339939c0ee049e439030f0dc72d46001
busenurdmb <bus.dmb@gmail.com>
2024-03-27 05:29:55 +0300

    Discount mikroservisi eklendi. Couople sınıfı oluşturuldu.  EntityFramework İLE SQL bağlanarak database ve  tablo oluşturuldu. Dapper ormsi kullanılarak crud işlemleri yapılarak api ayağa kaldırıldı.

---
+++ Services/Discount/MultiShop.Discount/Context/DiscountDapperContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DiscountDapperContext:DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DiscountDapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-493DFJA\\SQLEXPRESS;initial Catalog=MultiShopDiscountDb;integrated Security=true;TrustServerCertificate=True;");
+        }
+        public DbSet<Coupon> Coupons{ get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
